cada46349a6eb99f6fec88a7c74327ed32601410
Guillaume Sousa <guillaume.sousa@nist.gov>
2017-09-26 17:28:01 -0400

    Add error management
    Change-Id: I1d26621cf8fe1060982fd937af36a0ec65dcf3c4

--- core_explore_keyword_app/forms.py
+++ core_explore_keyword_app/forms.py
@@ -7,5 +7,5 @@ class KeywordForm(forms.Form):
     """
     Search by Keyword form
     """
-    keywords = forms.CharField(widget=forms.TextInput())
+    keywords = forms.CharField(widget=forms.TextInput(), required=False)
     query_id = forms.CharField(widget=forms.HiddenInput())
--- core_explore_keyword_app/views/user/views.py
+++ core_explore_keyword_app/views/user/views.py
@@ -5,6 +5,7 @@ import json
 from core_explore_common_app.components.query import api as query_api
 from core_explore_common_app.components.query.models import Query
 from core_explore_keyword_app.forms import KeywordForm
+from core_main_app.commons.exceptions import DoesNotExist
 from core_main_app.utils.databases.pymongo_database import get_full_text_query
 from core_main_app.utils.rendering import render
 
@@ -46,16 +47,34 @@ def keyword_search(request):
         "core_main_app/common/modals/error_page_modal.html"
     ]
 
+    error = None
+
     if request.method == 'POST':
         search_form = KeywordForm(request.POST)
+        # validate form
         if search_form.is_valid():
-            # get form values
-            query_id = request.POST['query_id']
-            keywords = request.POST['keywords']
-            # update query
-            query = query_api.get_by_id(query_id)
-            query.content = json.dumps(get_full_text_query(keywords))
-            query_api.upsert(query)
+            try:
+                # get form values
+                query_id = request.POST.get('query_id', None)
+                keywords = request.POST.get('keywords', None)
+
+                if query_id is None or keywords is None:
+                    error = "Expected parameters are not provided"
+                else:
+                    # get query
+                    query = query_api.get_by_id(query_id)
+                    if len(query.data_sources) == 0:
+                        error = "Please select at least 1 data source."
+                    else:
+                        # update query
+                        query.content = json.dumps(get_full_text_query(keywords))
+                        query_api.upsert(query)
+            except DoesNotExist:
+                error = "An unexpected error occurred while retrieving the query."
+            except Exception, e:
+                error = "An unexpected error occurred: {}.".format(e.message)
+        else:
+            error = "An unexpected error occurred: the form is not valid."
     else:
         # create query
         query = Query(user_id=str(request.user.id), templates=[])
@@ -66,6 +85,7 @@ def keyword_search(request):
     context = {
         'search_form': search_form,
         'query_id': search_form.data['query_id'],
+        'error': error,
         'local_query_url': 'core_explore_common_local_query',
         'data_sources_selector_template': 'core_explore_common_app/user/selector/data_sources_selector.html'
     }
