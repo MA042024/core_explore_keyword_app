f1a4c275ac1a41aeaf59d22f8ade4a4c638c4d86
Guillaume Sousa Amaral <guillaume.sousa@nist.gov>
2017-11-16 08:21:02 -0500

    Add missing model for permissions
    Change-Id: If83895e737565e5559bf4eaeaffcd2a9c4cc3ec0

---
+++ core_explore_keyword_app/models.py
@@ -0,0 +1,14 @@
+"""Explore Keyword models
+"""
+from django.db import models
+from core_main_app.permissions.utils import get_formatted_name
+from permissions import rights
+
+
+class ExploreKeyword(models.Model):
+    class Meta:
+        verbose_name = 'core_explore_keyword_app'
+        default_permissions = ()
+        permissions = (
+            (rights.explore_keyword_access, get_formatted_name(rights.explore_keyword_access)),
+        )
