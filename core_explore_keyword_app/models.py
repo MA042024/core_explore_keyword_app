d6e32ca9dd43c20d79a1aec87b80978778e8de3e
Philippe Dessauw <philippe.dessauw@nist.gov>
2017-11-27 19:41:54 -0500

    fix: clean reqs and imports
    Change-Id: I90bcc35ff2e782d508b74ebde4c95091e4181f74

--- core_explore_keyword_app/models.py
+++ core_explore_keyword_app/models.py
@@ -2,7 +2,7 @@
 """
 from django.db import models
 from core_main_app.permissions.utils import get_formatted_name
-from permissions import rights
+from core_explore_keyword_app.permissions import rights
 
 
 class ExploreKeyword(models.Model):
