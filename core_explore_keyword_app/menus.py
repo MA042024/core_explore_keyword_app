0483ea8ac85bc362086d21ab4b3ecf7f8c65dd20
Pierre Francois RIGODIAT <pierre-francois.rigodiat@nist.gov>
2018-05-09 08:53:16 -0400

    refactor: Make the text menu configurable
    Change-Id: I834bc8d3a2c2bdba1605c31b2dd1d01aca4f6cf9

--- core_explore_keyword_app/menus.py
+++ core_explore_keyword_app/menus.py
@@ -4,7 +4,9 @@
 from django.core.urlresolvers import reverse
 from menu import Menu, MenuItem
 
+from core_explore_keyword_app.settings import EXPLORE_MENU_NAME
+
 # FIXME: CHECK AUTHENTICATION !
 Menu.add_item(
-    "main", MenuItem("Query by Keyword", reverse("core_explore_keyword_app_search"))
+    "main", MenuItem(EXPLORE_MENU_NAME, reverse("core_explore_keyword_app_search"))
 )
--- core_explore_keyword_app/settings.py
+++ core_explore_keyword_app/settings.py
@@ -2,3 +2,6 @@ from django.conf import settings
 
 if not settings.configured:
     settings.configure()
+
+# MENU
+EXPLORE_MENU_NAME = getattr(settings, 'EXPLORE_MENU_NAME', 'Query by Keyword')
