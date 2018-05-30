5b9c5d5e2ff3621adbfbe16623c87a0fb24b56ec
Adrien Catel <adrien.catel@nist.gov>
2018-05-30 13:45:00 -0400

    chore: update beta3
    Change-Id: I66c2647ff9c300f30ea74440a6d71045bacff1f1

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ chdir(normpath(join(abspath(__file__), pardir)))
 
 setup(
     name="core_explore_keyword_app",
-    version="1.0.0-beta2",
+    version="1.0.0-beta3",
     description="Exploration by keywords for the curator core project",
     long_description=long_desc,
     author="NIST IT Lab",
