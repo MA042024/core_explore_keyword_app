594e85d3871a8c69eb5967a65102d5449259b706
Adrien Catel <adrien.catel@nist.gov>
2018-12-10 11:29:33 -0500

    chore: upgrade version 1.0.0
    Change-Id: I36a3e7210cb20b0c5c80f94956ed73f9036366dd

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ chdir(normpath(join(abspath(__file__), pardir)))
 
 setup(
     name="core_explore_keyword_app",
-    version="1.0.0-rc2",
+    version="1.0.0",
     description="Exploration by keywords for the curator core project",
     long_description=long_desc,
     author="NIST IT Lab",
