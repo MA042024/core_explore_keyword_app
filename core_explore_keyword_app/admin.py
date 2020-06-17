c717cf8e6d620befb17056b0026de1d4112447cb
Guillaume Sousa Amaral <guillaume.sousa@nist.gov>
2020-06-17 17:06:12 -0400

    fix: add missing staff auth to ajax
    Change-Id: I6365c2205383c76ec188877a7fb239d44ebd9075

--- core_explore_keyword_app/admin.py
+++ core_explore_keyword_app/admin.py
@@ -1,6 +1,7 @@
 """ Url router for administration views
 """
 from django.contrib import admin
+from django.contrib.admin.views.decorators import staff_member_required
 from django.urls import re_path
 
 from core_explore_keyword_app.views.admin import views as admin_views
@@ -14,12 +15,12 @@ admin_urls = [
     ),
     re_path(
         r"^operators/edit$",
-        admin_ajax.SearchOperatorConfigModalView.as_view(),
+        staff_member_required(admin_ajax.SearchOperatorConfigModalView.as_view()),
         name="core_explore_keyword_app_search_operator_config_modal",
     ),
     re_path(
         r"^operators/delete$",
-        admin_ajax.SearchOperatorDeleteModalView.as_view(),
+        staff_member_required(admin_ajax.SearchOperatorDeleteModalView.as_view()),
         name="core_explore_keyword_app_search_operator_delete_modal",
     ),
 ]
