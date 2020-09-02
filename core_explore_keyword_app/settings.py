b0aa0627f95ca796f404458851d52be4dde295ab
Guillaume Sousa Amaral <guillaume.sousa@nist.gov>
2020-09-01 20:46:43 -0400

    docs: add settings doc
    Change-Id: I933b14eefad4d9163001e515e898c9c11662a33b

--- core_explore_keyword_app/settings.py
+++ core_explore_keyword_app/settings.py
@@ -1,3 +1,9 @@
+""" Settings for core_explore_keyword_app
+
+Settings with the following syntax can be overwritten at the project level:
+SETTING_NAME = getattr(settings, "SETTING_NAME", "Default Value")
+"""
+
 from django.conf import settings
 
 if not settings.configured:
