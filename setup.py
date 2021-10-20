aba8cb4ea85b36fb7ad3dd5f8456fb12623100ec
Guillaume Sousa <guillaume.sousa@nist.gov>
2021-10-20 02:10:48 +0000

    chore: upgrade 1.18.0
    Change-Id: I8a2f046d2873fa9d990d0247824e4357e6c7a29c

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ chdir(normpath(join(abspath(__file__), pardir)))
 
 setup(
     name="core_explore_keyword_app",
-    version="1.17.0",
+    version="1.18.0",
     description="Exploration by keywords for the curator core project",
     long_description=long_desc,
     author="NIST IT Lab",
